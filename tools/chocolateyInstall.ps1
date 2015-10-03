b945e5622dc2707d84e859a9025a330829e98a42
Florian Rappl <rappl.florian@gmail.com>
2015-10-03 16:32:40 +0200

    updated to v0.3.11

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = 'Julia'
 $installerType = 'exe'
-$url32 = 'https://s3.amazonaws.com/julialang/bin/winnt/x86/0.3/julia-0.3.10-win32.exe'
-$url64 = 'https://s3.amazonaws.com/julialang/bin/winnt/x64/0.3/julia-0.3.10-win64.exe'
+$url32 = 'https://s3.amazonaws.com/julialang/bin/winnt/x86/0.3/julia-0.3.11-win32.exe'
+$url64 = 'https://s3.amazonaws.com/julialang/bin/winnt/x64/0.3/julia-0.3.11-win64.exe'
 $silentArgs = "/S"
  
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url32" "$url64"
